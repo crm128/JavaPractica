01b2b96edb0781d5bdfc34f03bfe76f58ca77b95
Cris <medina.cristian.rodolfo@gmail.com>
2023-05-20 00:25:30 -0300

    test ProblemasHackerRank

--- Problema22/index.java
+++ Problema22/index.java
@@ -3,7 +3,19 @@ package Problema22;
 public class index {
 
     public static void main(String[] args) {
-        
+        int n = 5;
+        for (int i = 1; i < n+1; i++) {
+            if (i%3==0 && i%5==0) {
+                System.out.println("FizzBuzz");
+            } else if (i%3==0) {
+                System.out.println("Fizz");
+            } else if (i%5==0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            } 
+        }
+
 
     }
     
---
+++ ProblemasHackerRank/MockTest1/test1.java
@@ -0,0 +1,19 @@
+package ProblemasHackerRank.MockTest1;
+
+import java.util.Arrays;
+
+public class test1 {
+    
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arreglo = new int[7];
+        Arrays.sort(arreglo);
+        while (i<) {
+            
+        } 
+    }
+
+
+
+}
